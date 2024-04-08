07438aec76ac50b3b1d9fd022be78ac75ecc816d
Aryan Daftari <aryandaftari7@gmail.com>
2024-04-08 17:57:38 +0530

    Added react

---
+++ 01- Inception/App.js
@@ -0,0 +1,37 @@
+const parent = React.createElement(
+    "div", {id:"parent"}, 
+    React.createElement
+    (
+        "div", {id:"child"},
+
+        React.createElement("h1", {}, "I am in H1 tag")
+    )
+);
+
+
+const heading = React.createElement("h1", {id:"heading"}, "Namaste React from React CDN");
+// {} where you will give attribute the tags
+// Third Attribute is the children
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// console.log(heading);
+// console.log(root);
+
+console.log(parent);
+
+//root.render(heading);
+
+root.render(parent);
+
+
+// The most costly operation in the Web Page is the DOM Manipulation.
+
+{/* <div id="parent">
+    <div id="child">
+        <h1>I am in H1 tag</h1>
+    </div>
+</div> */}
+
+
+// Render method is converting this heading object into HTML element
+// ReactElement(Object) => HTML(Browser Undreadable)
