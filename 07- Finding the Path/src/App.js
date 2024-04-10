01ad093fb335acacc90ea7bb0db37416044267f2
Aryan Daftari <aryandaftari7@gmail.com>
2024-04-10 13:38:47 +0530

    Implemented React Router

--- 07- Finding the Path/src/App.js
+++ 07- Finding the Path/src/App.js
@@ -2,22 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
+import { createBrowserRouter, RouterProvider , Outlet} from "react-router-dom";
 
 const AppLayout = () => {
 
     return (
             <div className="app">
                 <Header />
-                <Body />
+                <Outlet />
             </div>
     )
 }
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+                errorElement: <Error />
+            }
+            ,
+            {
+                path: "/about",
+                element: <About />,
+                errorElement: <Error />
+            }
+            ,
+            {
+                path: "/contact",
+                element: <Contact />,
+                errorElement: <Error />
+            }
+        ],
+        errorElement: <Error />
+    }
+    ,
+    
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={router} />);
 
 
--- 07- Finding the Path/src/components/Body.js
+++ 07- Finding the Path/src/components/Body.js
@@ -20,7 +20,7 @@ const Body = () => {
 
       const resData = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
 
-      console.log(resData)
+
       setListOfRestaurants(resData)
       setFilteredRestaurant(resData)
 
@@ -29,12 +29,11 @@ const Body = () => {
     }
     
     const handleClick = () => {
-        let filteredList = listOfRestaurants.filter((res) => res?.info?.avgRating >= 4.5)
+        let filteredList = listOfRestaurants.filter((res) => res?.info?.avgRating >= 4.0)
        
         setFilteredRestaurant(filteredList);
 
-        console.log(listOfRestaurants.length,"ListOfRes len - ")
-        console.log(filteredRestaurant.length,"FilOfRes len - ")
+        
     }
     const handleSearch = () => {
       const filteredRestaurant = listOfRestaurants.filter((res) => {
@@ -49,8 +48,7 @@ const Body = () => {
       setFilteredRestaurant(listOfRestaurants)
       setSearchText("")
 
-      console.log(listOfRestaurants.length,"ListOfRes len - ")
-        console.log(filteredRestaurant.length,"FilOfRes len - ")
+      
   }
 
     const handleSearchChange = (e) => {
